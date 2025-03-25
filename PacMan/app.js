fbe6364e82d450d51f1492e77f217c1966581d04
Ingthor06 <ingthor.logi@gmail.com>
2025-03-25 16:59:16 +0000

    pacman mouth movements added

--- PacMan/app.js
+++ PacMan/app.js
@@ -5,17 +5,28 @@ let ctx = canvas.getContext('2d');
 const colorYellow = 'rgba(255, 206, 86, 1)';
 const radius = 100;
 const angle = Math.PI / 180;
+let mouthOpen = true;
 
 function drawPacMan() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     ctx.beginPath();
     ctx.strokeStyle = colorYellow;
     ctx.fillStyle = colorYellow;
     ctx.lineWidth = 3;
 
-    ctx.arc(350, 300, 100, angle * 30, angle * 330, false);
+    let startAngle = mouthOpen ? angle * 30 : angle * 5;
+    let endAngle = mouthOpen ? angle * 330 : angle * 355;
+
+    ctx.arc(350, 300, radius, startAngle, endAngle, false);
     ctx.lineTo(350, 300);
     ctx.closePath();
 
     ctx.stroke();
     ctx.fill();
+    
+    mouthOpen = !mouthOpen;
 }
+
+setInterval(drawPacMan, 350)
+drawPacMan()
