8636bf58b3f584aa70f3971ac03a7f8f1a33d9bc
Ingthor06 <ingthor.logi@gmail.com>
2025-03-25 16:51:56 +0000

    github bug fixed

--- PacMan/app.js
+++ PacMan/app.js
@@ -6,14 +6,16 @@ const colorYellow = 'rgba(255, 206, 86, 1)';
 const radius = 100;
 const angle = Math.PI / 180;
 
-ctx.beginPath();
-ctx.strokeStyle = colorYellow;
-ctx.fillStyle = colorYellow;
-ctx.lineWidth = 3;
+function drawPacMan() {
+    ctx.beginPath();
+    ctx.strokeStyle = colorYellow;
+    ctx.fillStyle = colorYellow;
+    ctx.lineWidth = 3;
 
-ctx.arc(350, 300, 100, angle * 30, angle * 330, false);
-ctx.lineTo(350, 300);
-ctx.closePath();
+    ctx.arc(350, 300, 100, angle * 30, angle * 330, false);
+    ctx.lineTo(350, 300);
+    ctx.closePath();
 
-ctx.stroke();
-ctx.fill();
+    ctx.stroke();
+    ctx.fill();
+}
