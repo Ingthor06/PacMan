b30848b63157566f664c479a4dca64c3d35bd1e7
Ingthor06 <ingthor.logi@gmail.com>
2025-03-26 11:16:19 +0000

    pacman mouth to all directions added

--- PacMan/app.js
+++ PacMan/app.js
@@ -3,30 +3,100 @@ let canvas = document.getElementById('tutorial');
 let ctx = canvas.getContext('2d');
 
 const colorYellow = 'rgba(255, 206, 86, 1)';
-const radius = 100;
 const angle = Math.PI / 180;
 let mouthOpen = true;
 
-function drawPacMan() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.beginPath();
-    ctx.strokeStyle = colorYellow;
-    ctx.fillStyle = colorYellow;
-    ctx.lineWidth = 3;
-
-    let startAngle = mouthOpen ? angle * 30 : angle * 5;
-    let endAngle = mouthOpen ? angle * 330 : angle * 355;
-
-    ctx.arc(350, 300, radius, startAngle, endAngle, false);
-    ctx.lineTo(350, 300);
-    ctx.closePath();
-
-    ctx.stroke();
-    ctx.fill();
-    
-    mouthOpen = !mouthOpen;
+let pacmanState = {
+    direction: "right",
+    mouthOpen: true
 }
 
-setInterval(drawPacMan, 350)
-drawPacMan()
+function mouthAngles() {
+    if (pacmanState.direction === "lookRight") {
+        return {
+            start: pacmanState.mouthOpen ? angle * 30 : angle * 5,
+            end: pacmanState.mouthOpen ? angle * 330 : angle * 355
+        };
+    } 
+    else if (pacmanState.direction === "lookLeft"){
+        return {
+            start: pacmanState.mouthOpen ? angle * 210 : angle * 185,
+            end: pacmanState.mouthOpen ? angle * 150 : angle * 175
+        };
+    }
+    else if (pacmanState.direction === "lookUp") {
+        return {
+            start: pacmanState.mouthOpen ? angle * 300 : angle * 275,
+            end: pacmanState.mouthOpen ? angle * 240 : angle * 265            
+        };
+    }
+    else {
+        return {
+            start: pacmanState.mouthOpen ? angle * 120 : angle * 100,
+            end: pacmanState.mouthOpen ? angle * 55 : angle * 85            
+        };
+    }
+}
+
+
+const drawPacMan = {
+    x: 100,
+    y: 100,
+    vx: 10,
+    vy: 10,
+    radius: 25,
+    color: colorYellow,
+ 
+
+    draw() {
+        let { start, end } = mouthAngles();
+        
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, start, end, false);
+        ctx.lineTo(this.x, this.y);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+};
+
+
+function toggleMouth() {
+    pacmanState.mouthOpen = !pacmanState.mouthOpen;
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPacMan.draw();
+    window.requestAnimationFrame(gameLoop);
+}
+
+
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+        drawPacMan.y -= drawPacMan.vy
+        pacmanState.direction = 'lookUp';
+        break;
+    case 'ArrowDown':
+        drawPacMan.y += drawPacMan.vy
+        pacmanState.direction = 'lookDown';
+        break;
+    case 'ArrowLeft':
+        drawPacMan.x -= drawPacMan.vx
+        pacmanState.direction = 'lookLeft';
+        break;
+    case 'ArrowRight':
+        drawPacMan.x += drawPacMan.vx
+        pacmanState.direction = 'lookRight';
+        break;
+    default:
+        return;
+    }
+    gameLoop();
+})
+
+
+gameLoop();
+setInterval(toggleMouth, 100)
