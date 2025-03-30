4d0fde982d7a89cec096965dfaaae168054b37a8
Ingthor06 <ingthor.logi@gmail.com>
2025-03-30 14:36:24 +0000

    map fixed and positioned

--- PacMan/app.js
+++ PacMan/app.js
@@ -11,7 +11,7 @@ if (!canvas) {
     const colorCyan = 'rgba(20, 205, 200, 1)';
     const colorOrange = 'rgba(242, 121, 53, 1)';
     const angle = Math.PI / 180;
-    let tileSize = 30;
+    let tileSize = 40;
     let isPaused = false;
     const fullScreenButton =document.getElementById('fullScreenButton');
 
@@ -34,12 +34,12 @@ if (!canvas) {
         switch (pacmanState.direction) {
             case "lookRight":
                 return { start: pacmanState.mouthOpen ? angle * 30 : angle * 5, end: pacmanState.mouthOpen ? angle * 330 : angle * 355 };
-            case "lookLeft":
-                return { start: pacmanState.mouthOpen ? angle * 210 : angle * 185, end: pacmanState.mouthOpen ? angle * 150 : angle * 175 };
+            case "lookDown":
+                return { start: pacmanState.mouthOpen ? angle * 120 : angle * 100, end: pacmanState.mouthOpen ? angle * 55 : angle * 85 };
             case "lookUp":
                 return { start: pacmanState.mouthOpen ? angle * 300 : angle * 275, end: pacmanState.mouthOpen ? angle * 240 : angle * 265 };
             default:
-                return { start: pacmanState.mouthOpen ? angle * 120 : angle * 100, end: pacmanState.mouthOpen ? angle * 55 : angle * 85 };
+                return { start: pacmanState.mouthOpen ? angle * 210 : angle * 185, end: pacmanState.mouthOpen ? angle * 150 : angle * 175 };
         }
     }
 
@@ -84,7 +84,6 @@ if (!canvas) {
             this.ctx.arc(this.x, this.y, this.radius, Math.PI, 0, false);
             this.ctx.lineTo(this.x + this.radius, this.y + this.radius);
     
-            // Draw feet smoothly
             let footRadius = this.radius / 5;
             let footCount = 3;
             let footWidth = (this.radius * 2) / footCount;
@@ -120,13 +119,11 @@ if (!canvas) {
             this.ctx.arc(this.x + eyeOffsetX, this.y - eyeOffsetY, eyeRadius, 0, Math.PI * 2);
             this.ctx.fill();
 
-
-            this.ctx.fillStyle = "black";
+            this.ctx.fillStyle = "blue";
             this.ctx.beginPath();
             this.ctx.arc(this.x - eyeOffsetX - pupilRadius, this.y - eyeOffsetY, pupilRadius, 0, Math.PI * 2);
             this.ctx.fill();
 
-            // Right Pupil
             this.ctx.beginPath();
             this.ctx.arc(this.x + eyeOffsetX - pupilRadius, this.y - eyeOffsetY, pupilRadius, 0, Math.PI * 2);
             this.ctx.fill();
@@ -137,12 +134,12 @@ if (!canvas) {
     let ghostPositionX = Math.floor(Math.random() * 600) + 50;
     let ghostPositionY = Math.floor(Math.random() * 600) + 50;
 
-    const pacMan = new PacMan(ctx, 100, 100, 5, 5, 12, colorYellow);
+    const pacMan = new PacMan(ctx, 300, 380, 5, 5, 15, colorYellow);
     const ghosts = [
-        new Ghost(ctx, 200, 300, 12, colorRed),
-        new Ghost(ctx, 300, 400, 12, colorCyan),
-        new Ghost(ctx, 400, 200, 12, colorPink),
-        new Ghost(ctx, 500, 300, 12, colorOrange)
+        new Ghost(ctx, 300, 220, 15, colorRed),
+        new Ghost(ctx, 260, 300, 15, colorCyan),
+        new Ghost(ctx, 300, 300, 15, colorPink),
+        new Ghost(ctx, 340, 300, 15, colorOrange)
     ];
 
     function drawGhosts() {
@@ -158,21 +155,22 @@ if (!canvas) {
             this.height = height;
             this.tileSize = tileSize;
             this.map = [
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 2, 0, 1, 0, 2, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 2, 0, 1, 0, 2, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 2, 0, 1, 0, 2, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+                [1, 2, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 2, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+                [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+                [1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+                [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+                [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+                [1, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 1],
+                [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                
             ];
         }
     
@@ -180,17 +178,20 @@ if (!canvas) {
             this.map.forEach((row, y) => {
                 row.forEach((tile, x) => {
                     if (tile === 1) {
-                        this.ctx.fillStyle = "blue"; // Wall color
+                        this.ctx.fillStyle = "blue"; 
                         this.ctx.fillRect(x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);
+                        this.ctx.fillStyle = "black"; 
+                        this.ctx.fillRect(x * this.tileSize, y * this.tileSize, this.tileSize / 8, this.tileSize/ 8);
+                        
                     } else if (tile === 0) {
-                        this.ctx.fillStyle = "white"; // Empty space color
+                        this.ctx.fillStyle = "white"; 
                         this.ctx.beginPath();
                         this.ctx.arc(x * this.tileSize + this.tileSize / 2, y * this.tileSize + this.tileSize / 2, 3, 0, Math.PI * 2);
                         this.ctx.fill();
                     } else if (tile === 2) {
-                        this.ctx.fillStyle = "gold"; // Dot color
+                        this.ctx.fillStyle = "gold"; 
                         this.ctx.beginPath();
-                        this.ctx.arc(x * this.tileSize + this.tileSize / 2, y * this.tileSize + this.tileSize / 2, 5, 0, Math.PI * 2);
+                        this.ctx.arc(x * this.tileSize + this.tileSize / 2, y * this.tileSize + this.tileSize / 2, 8, 0, Math.PI * 2);
                         this.ctx.fill();
                     }
                 });
